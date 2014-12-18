092d7215b026ad65e27a601800ef5a4c046fbbc3
Zach Garwood <zachgarwood@gmail.com>
2014-12-18 11:06:55 -0600

    Completed ItemSearch operation functionality
    Also, added test request signing

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Add your AWS authentication credentials to `config/config.exs`:
 
 ```
 config :amazon_product_advertising_client,
+  associate_tag: "YourAssociateTag",
   aws_access_key_id: "YourAccessKeyID",
   aws_secret_access_key: "YourSecretAccessKey"
 ```
--- lib/amazon_product_advertising_client.ex
+++ lib/amazon_product_advertising_client.ex
@@ -1,43 +1,47 @@
 defmodule AmazonProductAdvertisingClient do
+  alias AmazonProductAdvertisingClient.Config
+
   use HTTPoison.Base
   use Timex
 
-  @url "http://webservices.amazon.com/onca/xml"
-  @params %{
-    "Service"         => "AWSECommerceService",
-    "Version"         => "2013-08-01",
-    "AWSAccessKeyId"  => Application.get_env(:amazon_product_advertising_client, :aws_access_key_id),
-    "AssociateTag"    => Application.get_env(:amazon_product_advertising_client, :associate_tag),
-  }
+  @scheme "http"
+  @host   "webservices.amazon.com"
+  @path   "/onca/xml"
 
-  def item_search(search_term) do
-    call_api %{"Operation" => "ItemSearch", "SearchIndex" => "Apparel", "Keywords" => search_term} 
+  def call_api(request_params, config \\ %Config{}) do
+    query = [request_params, config] |> combine_params |> URI.encode_query
+    get %URI{scheme: @scheme, host: @host, path: @path, query: query}
+  end
+
+  defp combine_params(params_list) do
+    List.foldl params_list, Map.new, fn(params, all_params) ->
+      Map.merge Map.from_struct(params), all_params
+    end
   end
 
   def process_url(url) do
-    sign_request url
+    url |> URI.parse |> timestamp_url |> sign_url |> String.Chars.to_string
   end
 
-  defp call_api(params) do
-    params = Map.put params, "Timestamp", DateFormat.format!(Date.local, "{ISOz}")
-    query = Map.merge(@params, params) |> Enum.sort |> URI.encode_query
-    get "#{@url}?#{query}"
+  defp timestamp_url(url_parts) do
+    timestamped_query = url_parts.query
+                        |> URI.decode_query
+                        |> Map.put_new("Timestamp", DateFormat.format!(Date.local, "{ISOz}"))
+                        |> URI.encode_query
+    Map.put url_parts, :query, timestamped_query
   end
 
-  defp sign_request(url) do
+  defp sign_url(url_parts) do
+    ordered_query = url_parts.query |> URI.decode_query |> Enum.sort |> URI.encode_query
     signature =
       :crypto.hmac(
         :sha256,
         Application.get_env(:amazon_product_advertising_client, :aws_secret_access_key),
-        construct_request url
+        Enum.join(["GET", url_parts.host, url_parts.path, ordered_query], "\n")
       )
       |> Base.encode64
       |> URI.encode_www_form
-    "#{url}&Signature=#{signature}"
-  end
-
-  defp construct_request(url) do
-    url_parts = URI.parse url
-    Enum.join ["GET", url_parts.host, url_parts.path, url_parts.query], "\n"
+    signed_query = "#{url_parts.query}&Signature=#{signature}"
+    Map.put url_parts, :query, signed_query
   end
 end
