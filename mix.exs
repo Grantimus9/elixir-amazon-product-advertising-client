8679320b12b7266f3f091dfc822d73d4b1eede35
Zach Garwood <zachgarwood@gmail.com>
2016-03-04 13:08:03 -0600

    Added Hex package metadata

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule AmazonProductAdvertisingClient.Mixfile do
     [app: :amazon_product_advertising_client,
      version: "0.1.0",
      elixir: "~> 1.0",
+     description: "An Amazon Product Advertising API client for Elixir",
+     package: package,
      deps: deps]
   end
 
@@ -17,6 +19,14 @@ defmodule AmazonProductAdvertisingClient.Mixfile do
       {:httpoison, "~> 0.8.1"},
       {:sweet_xml, "~> 0.6.1"},
       {:timex, "~> 1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/zachgarwood/elixir-amazon-product-advertising-client"},
+      maintainers: ["Zach Garwood <zachgarwood@gmail.com>"]
     ]
   end
 end
