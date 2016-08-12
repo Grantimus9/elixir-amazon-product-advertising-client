eed1b478e8b17baf2ac7fffe63ee1e81487a5dcb
Zach Garwood <zachgarwood@gmail.com>
2016-08-12 14:34:26 -0500

    Updated struct formatting

--- lib/amazon_product_advertising_client/config.ex
+++ lib/amazon_product_advertising_client/config.ex
@@ -1,11 +1,6 @@
 defmodule AmazonProductAdvertisingClient.Config do
-
-  def __struct__ do
-    %{
-      "AssociateTag"    => Application.get_env(:amazon_product_advertising_client, :associate_tag),
-      "AWSAccessKeyId"  => Application.get_env(:amazon_product_advertising_client, :aws_access_key_id),
-      "Service"         => "AWSECommerceService",
-      "Version"         => "2013-08-01",
-    }
-  end
+  defstruct "AssociateTag": Application.get_env(:amazon_product_advertising_client, :associate_tag),
+    "AWSAccessKeyId": Application.get_env(:amazon_product_advertising_client, :aws_access_key_id),
+    "Service": "AWSECommerceService",
+    "Version": "2013-08-01"
 end
