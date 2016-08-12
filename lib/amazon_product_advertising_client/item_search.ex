75dcaee708f29f1f756c17988de17befb1158fe3
Zach Garwood <zachgarwood@gmail.com>
2016-08-12 14:34:26 -0500

    Updated struct formatting

--- lib/amazon_product_advertising_client/item_search.ex
+++ lib/amazon_product_advertising_client/item_search.ex
@@ -2,22 +2,18 @@ defmodule AmazonProductAdvertisingClient.ItemSearch do
   alias __MODULE__
   alias AmazonProductAdvertisingClient.Config
 
-  def __struct__ do
-    %{
-      "Availability"  => "Available",
-      "BrowseNode"    => nil,
-      "Condition"     => "New",
-      "Keywords"      => nil,
-      "Operation"     => "ItemSearch",
-      "MaximumPrice"  => nil,
-      "MinimumPrice"  => nil,
-      "ResponseGroup" => nil,
-      "SearchIndex"   => nil,
-      "ResponseGroup" => nil,
-      "Title"         => nil,
-      "Sort"          => nil,
-    }
-  end
+  defstruct "Availability": "Available",
+    "BrowseNode": nil,
+    "Condition": "New",
+    "Keywords": nil,
+    "MaximumPrice": nil,
+    "MinimumPrice": nil,
+    "Operation": "ItemSearch",
+    "ResponseGroup": nil,
+    "ResponseGroup": nil,
+    "SearchIndex": nil,
+    "Sort": nil,
+    "Title": nil
 
   def execute(search_params \\ %ItemSearch{}, config \\ %Config{}) do
     AmazonProductAdvertisingClient.call_api search_params, config
