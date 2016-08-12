5a70f4c1c59876cf8f1754748685c753f888c1ed
Zach Garwood <zachgarwood@gmail.com>
2016-08-12 14:54:48 -0500

    Updated dependencies

--- mix.exs
+++ mix.exs
@@ -11,14 +11,20 @@ defmodule AmazonProductAdvertisingClient.Mixfile do
   end
 
   def application do
-    [applications: [:logger, :tzdata]]
+    [
+      applications: [
+        :httpoison,
+        :logger,
+        :tzdata,
+      ]
+    ]
   end
 
   defp deps do
     [
-      {:httpoison, "~> 0.8.1"},
-      {:sweet_xml, "~> 0.6.1"},
-      {:timex, "~> 1.0"}
+      {:httpoison, "~> 0.9"},
+      {:sweet_xml, "~> 0.6"},
+      {:timex, "~> 3.0"},
     ]
   end
 
