75a4c59de9f07e4d544b1020c266b88916d3249d
Zach Garwood <zachgarwood@gmail.com>
2017-01-20 14:29:21 -0600

    Added module and function docs

--- lib/amazon_product_advertising_client/item_search.ex
+++ lib/amazon_product_advertising_client/item_search.ex
@@ -1,8 +1,12 @@
 defmodule AmazonProductAdvertisingClient.ItemSearch do
-  @moduledoc false
+  @moduledoc """
+  The [ItemSearch](http://docs.aws.amazon.com/AWSECommerceService/latest/DG/ItemSearch.html) operation
+
+  """
 
   alias __MODULE__
   alias AmazonProductAdvertisingClient.Config
+
   defstruct "Availability": "Available",
       "BrowseNode": nil,
       "BrowseNodeId": nil,
@@ -17,6 +21,9 @@ defmodule AmazonProductAdvertisingClient.ItemSearch do
       "Sort": nil,
       "Title": nil
 
+  @doc """
+  Execute an ItemSearch operation
+  """
   def execute(search_params \\ %ItemSearch{}, config \\ %Config{}) do
     AmazonProductAdvertisingClient.call_api search_params, config
   end
